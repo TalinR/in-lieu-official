5679e3c6da36ac62e05fdd4aaf4216f74d63a812
Talin Roche <talin.roche@gmail.com>
2025-08-14 16:52:45 +1000

    got rid of sections context from the layout file (so it shouldnt be used anymore

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import '../styles/globals.css';
 import Navbar from '@/components/layout/navbar/Navbar';
-import { SectionsProvider } from '@/components/layout/SectionsContext';
 import { CartProvider } from '@/components/cart/cart-context';
 import { getCart } from '@/lib/shopify';
 import {
@@ -31,10 +30,8 @@ export default function RootLayout({
     <html lang="en">
       <body className="font-sans antialiased">
         <CartProvider cartPromise={cart}>
-          <SectionsProvider>
             <Navbar />
             {children}
-          </SectionsProvider>
         </CartProvider>
       </body>
     </html>
