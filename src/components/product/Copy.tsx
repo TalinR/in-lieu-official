6286d933c51a32378167ccd85ffdc684d2c7c2ef
Talin Roche <talin.roche@gmail.com>
2025-08-16 20:25:10 +1000

    changed from seo description to description

--- src/components/product/Copy.tsx
+++ src/components/product/Copy.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import type { Product } from '@/lib/shopify/types';
 
 export default function Copy({ product }: { product: Product }) {
-  const blurb = product.seo?.description || product.description || '';
+  const blurb = product?.description || product.description || '';
   return (
     <div className="space-y-3">
       <h1 className="text-4xl  font-regular text-black leading-tight tracking-[-0.02em]">{product.title}</h1>
